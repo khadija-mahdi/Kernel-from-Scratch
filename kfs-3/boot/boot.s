59dbde4fdacb262a630ca8bec13dcb49e7800c60
khadija-mahdi <khadijamahdi6@gmail.com>
2026-01-18 13:45:44 +0100

    add pagiing

--- kfs-3/boot/boot.s
+++ kfs-3/boot/boot.s
@@ -15,19 +15,53 @@ header_end:
 
 
 
-section .bss
+
+SECTION .bss
+ALIGN 16
 stack_bottom:
-    resb 16384
+    RESB 16384 * 8
 stack_top:
 
-section .text
-extern kernel_main
+section .boot
 
+global _start
 _start:
-    ; Set up stack pointer
-    mov esp, stack_top
-    
-    ; Call C kernel main function
-    call kernel_main
-    
-section .note.GNU-stack noalloc noexec nowrite progbits
+    MOV ecx, (initial_page_dir - 0xC0000000)
+    MOV cr3, ecx
+
+    MOV ecx, cr4
+    OR ecx, 0x10
+    MOV cr4, ecx
+
+    MOV ecx, cr0
+    OR ecx, 0x80000000
+    MOV cr0, ecx
+
+    JMP higher_half
+
+section .text
+higher_half:
+    MOV esp, stack_top
+    PUSH ebx
+    PUSH eax
+    XOR ebp, ebp
+    extern kernel_main
+    CALL kernel_main
+
+halt:
+    hlt
+    JMP halt
+
+
+section .data
+align 4096
+global initial_page_dir
+initial_page_dir:
+    DD 10000011b
+    TIMES 768-1 DD 0
+
+    DD (0 << 22) | 10000011b
+    DD (1 << 22) | 10000011b
+    DD (2 << 22) | 10000011b
+    DD (3 << 22) | 10000011b
+    TIMES 256-4 DD 0
